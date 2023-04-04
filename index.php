5d062b3c80c05e5491f7c42f4e15c09aedaecdca
alexandragre <aalexandragredeus@gmail.com>
2023-04-04 10:31:29 +0200

    nav
    nav

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+        <a href="index.php"><p>Sipper</a></p>
+        <ul>
+            <li><a href="login.php"></a></li>
+    </ul>
+    </div>
+    </nav>
+
+    <h1>HELLO</h1>
+    <h1>My First Heading</h1>
+</body>
+</html>
