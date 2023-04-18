0526c32b3ea5aa39dbb89e4afac0bee66f67db3b
alexandragre <aalexandragredeus@gmail.com>
2023-04-18 20:12:15 +0200

    css and edit page
    fix

--- edit.php
+++ edit.php
@@ -34,33 +34,41 @@
 <h3 class="create-h">Created by you</h3>
 
     <div class="recipes-overview">
-        <table>
-            <thead>
-                <th>Title</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </thead>
-    
-            <tbody>
-                <!--  <?php -->
-                     <!-- loops and output data of each row
-                    while ($row = $result->fetch_assoc()) { ?>-->
-                <tr>
-                <!-- Outputs the title and description-->
-                <td class="recipe"><!--<?php echo $row['drink_title']; ?>--></td>
-                    
-                <td class="update">
-                <!--<a href="edit-recipe.php?id=<?php echo $row['drink_title']; ?>"--> class="edit_btn">
-                <button class="btn-trash"><i class="fa fa-trash"></i></button></a>
-                </td>
-    
-                <td class="delete">
-                <button class="btn-pen"><i class="fa fa-pencil" aria-hidden="true"></i> </button>
-                </td>
-    
-                 <!-- <?php } ?>-->
-                </tr>
 
+    <table>
+		<thead>
+			<th>Title</th>
+			<th>Edit</th>
+			<th>Delete</th>
+		</thead>
+
+		<tbody>
+			<?php 
+                // loops and output data of each row
+                while ($row = $result->fetch_assoc()) { ?>
+			<tr>
+			<!-- Outputs the title and description-->
+			<td class="recipe"><?php echo $row['drink_title']; ?></td>
+                
+			<td class="update">
+			<a href="edit-recipe.php?id=<?php echo $row['drink_title']; ?>" class="edit_btn">
+            <button class="btn-trash"><i class="fa fa-trash"></i></button></a>
+			</td>
+
+            <td class="delete">
+            <button class="btn-pen"><i class="fa fa-pencil" aria-hidden="true"></i> </button>
+            </td>
+
+			<?php } ?>
+        	</tr>
+
+        <li class="recipe">Recipes</li>
+        <li>
+            <button class="btn-trash"><i class="fa fa-trash"></i></button>
+        </li>
+        <li>
+            <button class="btn-pen"><i class="fa fa-pencil" aria-hidden="true"></i> </button>
+        </li>
     </div>
 </section>
 
