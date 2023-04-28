138254bbc5091affd6a59e6a693d0ac102c9ff5a
kingazurawska <80899440+kingazurawska@users.noreply.github.com>
2023-04-28 15:18:47 +0200

    Model

---
+++ models/CocktailModel.php
@@ -0,0 +1,17 @@
+<?php
+
+// Check for a defined constant or specific file inclusion
+if (!defined('MY_APP') && basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    die('This file cannot be accessed directly.');
+}
+
+// Model class for customers-table in database
+
+class CocktailModel{
+    public $cocktail_id;
+    public $title;
+    public $description;
+    public $ingredients;
+    public $instructions;
+    public $image_url;
+}
