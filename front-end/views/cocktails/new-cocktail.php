6d47c54a1d60e170457a8867fa5fb6ff01f9270a
alexandragre <aalexandragredeus@gmail.com>
2023-05-05 13:26:38 +0200

    fix

--- front-end/views/cocktails/new-cocktail.php
+++ front-end/views/cocktails/new-cocktail.php
@@ -11,14 +11,16 @@ Template::header("New Cocktail");
     <h1>SHARE & TEST</h1>
 
     <form method="POST" action="create-cocktail.php" name="AddForm" onsubmit="return validateForm()">
-        <label for="drink-text" class="drink-text">Drink name</label>
-        <input type="text" id="drink-input" name="drink-input">
+        <label for="title" class="drink-text">Drink name</label>
+        <input type="title" id="drink-input" name="drink-input">
         <br><br>
         <label for="description" class="description-text">Drink description</label>
         <input type="text" id="description" name="description">
         <br><br>
-        <label for="ingredients" class="ingredients-text">Ingridients</label>
-        <input type="text" id="ingredients" name="ingridients">
+        <label for="ingredients" class="ingredients-text">Ingredients</label>
+        <input type="text" id="ingredients" name="ingredients">
+        <label for="image" class="image">image</label>
+        <input type="image" id="image" name="image">
         <input type="submit" class="input-create" value="Create">
       </form>
 
