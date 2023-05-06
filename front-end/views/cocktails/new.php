c4e8ed73c498e07d80d71593fb20986d26c984aa
kingazurawska <80899440+kingazurawska@users.noreply.github.com>
2023-05-06 12:42:13 +0200

    Create New Recipe 0.3

--- front-end/views/cocktails/new.php
+++ front-end/views/cocktails/new.php
@@ -6,26 +6,23 @@ require_once __DIR__ . "/../../Template.php";
 Template::header("New Cocktail");
 ?>
 
-    <div class="create-new">
-                <article class="heading">
-                    <h1 class="big-h">CREATE A <br> NEW RECIPIE..</h1>
-                </article>               
-            </div>
+<div class="create-new">                    
+            
+      <div class="form-left">
+        <h1 class="big-h">CREATE A <br> NEW RECIPIE..</h1>
+        <form action="<?= $this->home ?>/cocktails" method="POST" name="AddForm" onsubmit="return validateForm()">
+        <input class="title" type="text" name="title" placeholder="Title"> <br>
+        <input class="description" type="text" name="description" placeholder="Description"> <br>
+        <input class="ingredients" type="text" name="ingredients" placeholder="Ingredients"> <br>
+      </div>
 
-    <form action="<?= $this->home ?>/cocktails" method="POST" name="AddForm" onsubmit="return validateForm()">
-    <input class="title" type="text" name="title" placeholder="Title"> <br>
-    <input class="description" type="text" name="description" placeholder="Description"> <br>
-    <input class="ingredients" type="text" name="ingredients" placeholder="Ingredients"> <br>
-    <input class="instructions" type="text" name="instructions" placeholder="Instructions"> <br>
-    <!--<input class="image" type="image" name="image_url" placeholder="image_url"> <br>-->
-    <input type="Save" value="Save" class="btn">    
-    </form>
-    
+      <div class="form-right">
+        <input class="instructions" type="text" name="instructions" placeholder="Instructions"> <br>
+        <input class="image" type="file" id="img" name="image_url" placeholder="Add image" accept="image/*"> <br>
+        <input type="Save" value="Save" class="btn">    
+        </form>
+      </div>
 
-</section>
-
-<section class="share-img-container">
-<button class="share-img">Add Image</button>
-</section>
+</div>
 
 <?php Template::footer(); ?>
