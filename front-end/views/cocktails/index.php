c70ce1b669a6ea3517ff7de537ebf41788284ae3
alexandragre <aalexandragredeus@gmail.com>
2023-05-07 19:23:06 +0200

    style fix

--- front-end/views/cocktails/index.php
+++ front-end/views/cocktails/index.php
@@ -4,18 +4,19 @@ require_once __DIR__ . "/../../Template.php";
 Template::header("Cocktails");
 ?>
 
-<div class="landing-page">
              
-<article class="search-container">
+<div class="add-container">
             <button class="add-btn"><a href="<?= $this->home ?>/cocktails/new">Create new recipe</a></button>   
-</article>
+</div>
+
+<div class="landing-page">
 
 <div class="item-grid">
 
     <?php foreach ($this->model as $cocktail) : ?>
 
         <article class="item">
-            <div>
+            <div class="drink-card">
                 
                 <span>Title: <?= $cocktail->title ?></span> <br>
                 <span>Description: <?= $cocktail->description ?></span> <br>
@@ -25,8 +26,8 @@ Template::header("Cocktails");
             </div>
 
             <div class="show-edit">
-            <a href="<?= $this->home ?>/cocktails/<?= $cocktail->cocktail_id ?>">Show</a>
-            <a href="<?= $this->home ?>/cocktails/<?= $cocktail->cocktail_id ?>/edit">Edit</a>
+            <a href="<?= $this->home ?>/cocktails/<?= $cocktail->cocktail_id ?>" class="show-btn">Show</a>
+            <a href="<?= $this->home ?>/cocktails/<?= $cocktail->cocktail_id ?>/edit" class="edit-btn">Edit</a>
             </div>
         </article>
 
