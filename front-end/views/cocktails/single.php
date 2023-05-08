e420cc0287d7f0065a91a981c8bc1644f7746b0d
alexandragre <aalexandragredeus@gmail.com>
2023-05-08 11:21:41 +0200

    styling

--- front-end/views/cocktails/single.php
+++ front-end/views/cocktails/single.php
@@ -4,32 +4,42 @@ require_once __DIR__ . "/../../Template.php";
 Template::header($this->model->title);
 ?>
 
-<h1><?= $this->model->title ?></h1>
+<div class="landing-page">
+
+<div class="overview">
+<div class="cocktail-overview">
+
+<h1 class="c-title"><?= $this->model->title ?></h1>
 
 <p>
-    <b>Id: </b>
-    <?= $this->model->cocktail_id ?> 
+    <b class="cocktail-txt">Id: <?= $this->model->cocktail_id ?></b>
 </p>
 
 <p>
-    <b>Title: </b>
-    <?= $this->model->title ?> 
+    <b class="cocktail-txt">Title: <?= $this->model->title ?> </b>
 </p>
 
 <p>
-    <b>Description: </b>
-    <?= $this->model->description ?> 
+    <b class="cocktail-txt">Description: <?= $this->model->description ?> </b>
 </p>
 
 <p>
-    <b>Ingredients: </b>
-    <?= $this->model->ingredients ?> 
+    <b class="cocktail-txt">Ingredients: <?= $this->model->ingredients ?> </b>
 </p>
 
 <p>
-    <b>Instructions: </b>
-    <?= $this->model->instructions ?> 
+    <b class="cocktail-txt">Instructions: <?= $this->model->instructions ?> </b>
 </p>
+</div>
 
+<div class="img-overview">
+<p>
+<b><?= $this->model->image_url ?> </b>
+
+</p>
+</div>
+</div>
+
+</div>
 
 <?php Template::footer(); ?>
