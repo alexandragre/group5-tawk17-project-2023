c97365e61f5b4938d81d2369a5ca00fddfbb5978
alexandragre <aalexandragredeus@gmail.com>
2023-05-08 10:46:54 +0200

    style

--- front-end/views/cocktails/edit.php
+++ front-end/views/cocktails/edit.php
@@ -4,18 +4,36 @@ require_once __DIR__ . "/../../Template.php";
 Template::header("Edit " . $this->model->title);
 ?>
 
-<div class="create-new">    
                
+<div class="create-new">                    
+            
+      <div class="form-left">
+      <h1 class="big-h">EDIT RECIPE</h1>
+      <form action="<?= $this->home ?>/cocktails/<?= $this->model->cocktail_id ?>/edit" method="post">
+        <input class="title" type="text" name="title" value="<?= $this->model->title ?>"placeholder="Title"> <br>
+        <input class="description" type="text" name="description" value="<?= $this->model->description ?>"placeholder="Description"> <br>
+        <input class="ingredients" type="text" name="ingredients" value="<?= $this->model->ingredients ?>"placeholder="Ingredients"> <br>
+      </div>
 
+      <div class="form-right">
+      <input class="instructions" type="text" name="instructions" value="<?= $this->model->instructions ?>" placeholder="Instructions"> <br>
+    <input class="image" type="file" id="img" name="image_url" accept="image/*"value="<?= $this->model->image_url ?>" placeholder="image"> <br>
+          
+        <input type="submit" value="Save" class="save-btn">
+    <input type="submit" value="Delete" class="delete-btn">
+        </form>
+      </div>
+
+</div>
 <form action="<?= $this->home ?>/cocktails/<?= $this->model->cocktail_id ?>/edit" method="post">
 <div class="form-left">
     <h1 class="big-h">EDIT RECIPE</h1>
     <input class="title" type="text" name="title" value="<?= $this->model->title ?>" placeholder="Title"> <br>
     <input class="description" type="text" name="description" value="<?= $this->model->description ?>" placeholder="Description"> <br>
+    <input class="ingredients" type="text" name="ingredients" value="<?= $this->model->ingredients ?>" placeholder="Ingridients"> <br>
 </div>
     
 <div class="form-right">
-    <input class="ingredients" type="text" name="ingredients" value="<?= $this->model->ingredients ?>" placeholder="Ingridients"> <br>
     <input class="instructions" type="text" name="instructions" value="<?= $this->model->instructions ?>" placeholder="Instructions"> <br>
     <input class="image" type="file" id="img" name="image_url" accept="image/*"value="<?= $this->model->image_url ?>" placeholder="image"> <br>
     
@@ -23,7 +41,6 @@ Template::header("Edit " . $this->model->title);
     <input type="submit" value="Delete" class="delete-btn">
     </form>
     </div>
-</div> 
 
 
 <?php Template::footer(); ?>
