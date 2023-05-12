34aa0ac7e9d91fb3ca7ce6f3d4e718a8d70797a7
bellasundqvist <bellasundqvist@live.se>
2023-05-12 14:40:41 +0200

    update

--- front-end/views/cocktails/index.php
+++ front-end/views/cocktails/index.php
@@ -23,9 +23,19 @@ Template::header("Cocktails");
             
             </div>
 
+            <?php if ($this->user->user_setting === "admin") : ?>
+
+            <p>
+                <b>User ID: </b>
+                <?= $cocktail->user_id ?>
+            </p>
+
             <div class="show-edit">
-            <a href="<?= $this->home ?>/cocktails/single<?= $cocktail->cocktail_id ?>" class="show-btn">Show</a>
-            <a href="<?= $this->home ?>/cocktails/edit<?= $cocktail->cocktail_id ?>/edit" class="edit-btn">Edit</a>
+            <a href="<?= $this->home ?>/cocktails/<?= $cocktail->cocktail_id ?>/single" class="show-btn">Show</a>
+
+            <?php endif; ?>
+
+            <a href="<?= $this->home ?>/cocktails/<?= $cocktail->cocktail_id ?>/edit" class="edit-btn">Edit</a>
             </div>
         </article>
 
