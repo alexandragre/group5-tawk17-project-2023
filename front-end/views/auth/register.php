75d1e506a57a8e119632b046068c436db02330f9
bellasundqvist <bellasundqvist@live.se>
2023-05-18 14:13:14 +0200

    register

--- front-end/views/auth/register.php
+++ front-end/views/auth/register.php
@@ -4,13 +4,15 @@ require_once __DIR__ . "/../../Template.php";
 Template::header("Register user", $this->model["error"]);
 ?>
 
+<div class="center-container">
 <h1>Register user</h1>
 
 <form action="<?= $this->home ?>/auth/register" method="post">
-    <input type="text" name="username" placeholder="Username"> <br>
-    <input type="password" name="password" placeholder="Password"> <br>
-    <input type="password" name="confirm_password" placeholder="Confirm password"> <br>
-    <input type="submit" value="Save" class="btn">
+    <input class="field" type="text" name="username" placeholder="Username"> <br>
+    <input class="field" type="password" name="password" placeholder="Password"> <br>
+    <input class="field" type="password" name="confirm_password" placeholder="Confirm password"> <br>
+    <input class="save" type="submit" value="Save" class="btn">
 </form>
+</div>
 
 <?php Template::footer(); ?>
