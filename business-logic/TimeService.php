d08f463b420a5d1dafe95d64e8831a11f936ef6d
kingazurawska <80899440+kingazurawska@users.noreply.github.com>
2023-05-19 08:47:38 +0200

    .

---
+++ business-logic/TimeService.php
@@ -0,0 +1,7 @@
+<?php 
+
+// Check for a defined constant or specific file inclusion
+if (!defined('MY_APP') && basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    die('This file cannot be accessed directly.');
+}
+
